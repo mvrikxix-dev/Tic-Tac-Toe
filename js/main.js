b9d05ba539629520474a3e2048416ca928f1549c
mvrikxix-dev <mvrikxix-dev@gmail.com>
2020-05-03 16:22:30 +0530

    Redesigned

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
-let player = null;
+let player = "PLAYER1";
+var game_options = document.querySelector('.game-options');
+var add_name = document.querySelector('.onready-container');
+var content_container = document.querySelector('.content-container');
+var ingame_wrapper = document.querySelector('.wrapper-ingame');
 
-document.addEventListener('click',(e)=>{
-    let target = e.target;
-    let target_function = target.getAttribute('function')
+ingame_wrapper.classList.add("blank");
+add_name.classList.add("blank");
+content_container.classList.add("blank");
 
-    if(target_function=="close-absolute"){
-        close_absolute(target);
-    }
-
-    if(target_function=="setUserName"){
-        setUserName(target);
-    }
-    
+game_options.addEventListener('click', (e) => {
+    e.target.getAttribute('id') === "play-friend" ? playFriend() : playOnline();
 })
 
-function close_absolute(target){
-    let div = target.parentElement.parentElement.parentElement;
-    div.style.display="none";
+function playFriend() {
+    console.log("PvP");
+    game_options.classList.add("blank");
+    setUserName();
 }
 
-function setUserName(target){
-    let name = document.getElementById('userName');
-    player = name.value;
-    close_absolute(target.parentElement)
-    console.log(player);
+function playOnline() {
+    console.log("vsBot");
+    game_options.classList.add("blank");
+    setUserName();
+}
+
+function setUserName() {
+    add_name.classList.remove("blank");
+    add_name.addEventListener('click', (e) => {
+        if (e.target.className === "close-absolute") {
+            add_name.classList.add("blank");
+        } else if (e.target.id === "nameSubmit") {
+            player = document.getElementById('userName').value;
+            add_name.classList.add("blank");
+            console.log(player);
+            content_container.classList.remove("blank");
+            document.querySelector('.name-change').textContent = player;
+            ingame_wrapper.classList.remove("blank");
+        }
+    })
 }
